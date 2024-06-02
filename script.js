9d22870b746c47bed301229526eb8b99930c8eb3
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-06-03 02:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,29 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+
+    let arr = document.getElementsByClassName("price");
+    let sm=0;
+    for(let i=0;i<arr.length;i++){
+        sm+=parseInt(arr[i].innerHTML);
+    }
+
+    let ltc = document.createElement("td");
+    let rtc = document.createElement("td");
+
+    ltc.setAttribute("class","item");
+    rtc.setAttribute("class","price");
+
+    let pDiv = document.getElementById("tbl");
+    ltc.textContent = `Total Price`;
+    rtc.textContent = `${sm}`;
+
+    let tmpP = document.createElement("tr");
+
+    tmpP.appendChild(ltc);
+    tmpP.appendChild(rtc);
+
+    pDiv.appendChild(tmpP);
   
 };
 
